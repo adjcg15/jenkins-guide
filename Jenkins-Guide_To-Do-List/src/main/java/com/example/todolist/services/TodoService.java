f76ff5f5733e9d9ac3d2863885ca6c505fd5aa68
Angel Garcia <adjcg15@gmail.com>
2025-11-17 19:11:16 -0600

    feat(TodoService): implement dynamic filePath when creating service to handle test environment

--- Jenkins-Guide_To-Do-List/src/main/java/com/example/todolist/services/TodoService.java
+++ Jenkins-Guide_To-Do-List/src/main/java/com/example/todolist/services/TodoService.java
@@ -12,12 +12,20 @@ import java.util.List;
 @Service
 public class TodoService {
 
-    private final String FILE_PATH = "data/to_do.json";
+    private String filePath;
     private final ObjectMapper mapper = new ObjectMapper();
+
+    public TodoService() {
+        this.filePath = "data/to_do.json";
+    }
+
+    public TodoService(String filePath) {
+        this.filePath = filePath;
+    }
 
     public List<TodoModel> getAll() {
         try {
-            File file = new File(FILE_PATH);
+            File file = new File(this.filePath);
             if (!file.exists()) return new ArrayList<>();
             return mapper.readValue(file, new TypeReference<>() {});
         } catch (Exception e) {
@@ -42,7 +50,7 @@ public class TodoService {
 
     private void save(List<TodoModel> todos) {
         try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(FILE_PATH), todos);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(this.filePath), todos);
         } catch (Exception e) {
             e.printStackTrace();
         }
